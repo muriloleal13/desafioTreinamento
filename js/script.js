ac6dd4fc0fff98944f18f0a9b9a5decacae4984d
muriloleal13 <mukaleal@gmail.com>
2020-01-28 20:34:27 -0300

    code

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let products = [
+    {
+        name: "Maçã",
+        quantity: 25
+    },
+    {
+        name: "Banana",
+        quantity: 12
+    },
+    {
+        name: "Cenoura",
+        quantity: 13
+    }
+];
+
+let table = $("#table-produtos");
+let addedProduct = (alert, type) => {
+    document.getElementById('span_message').innerHTML = 
+        `<div class="alert alert-${alert}" role="alert" id="div1" style="display:none;">
+            ${type} to insert product!
+        </div>`;
+    $('#div1').fadeIn(2000);
+    $('#div1').fadeOut(3000);
+}
+
+function attTable() {
+    let newRow = $("<tr>");
+    let cols = "<th>Product</td><th>Quantity</td>";  
+    newRow.append(cols);
+    table.append(newRow);    
+    for(let i=0; i < products.length; i++) {
+        let newRow = $("<tr>");
+        let cols = `<td> ${products[i].name} </td>
+                    <td> ${products[i].quantity} </td>`;
+        newRow.append(cols);
+        table.append(newRow);
+    }
+}
+
+function limpaTabela(){
+    $("#table-produtos tr").remove();
+    $('#product').val('');
+    $('#quantity').val('');
+}
+
+function adicionaItem() {
+    let newProduct = $('#product').val();
+    let newQuantity = parseInt($('#quantity').val());
+    let arrExists = products.filter(elem => elem.name == newProduct);
+    console.log(`${newProduct} ${newQuantity} ${newProduct == ''} ${isNaN(newQuantity)}`);
+    if(((typeof(newProduct) !== 'string' || typeof(newQuantity) !== 'number') || newProduct == '' || isNaN(newQuantity))){
+        addedProduct('danger', 'Error');
+    }else if(arrExists.length == 0){
+        products = [
+            ...products,
+            { 
+                name: product.value,
+                quantity: quantity.value
+            }
+        ];
+        addedProduct('success', 'Success');
+    }else{
+        products.map(elem => {
+            if(elem.name == newProduct)
+                return elem.quantity = parseInt(elem.quantity) + parseInt(newQuantity);
+        });
+        addedProduct('warning', 'Warning');
+    }
+    limpaTabela();
+    attTable();
+}
+
+attTable();
